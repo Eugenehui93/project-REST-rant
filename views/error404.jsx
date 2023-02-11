d7e6197ed7a5f42fe9100845cb98bd1091155132
Yee Ching Hui <eugenehui93@Yees-MBP.lan>
2023-02-11 10:38:59 -0800

    Adding Bootstrap grid to index page

--- views/error404.jsx
+++ views/error404.jsx
@@ -7,6 +7,11 @@ function error404 () {
             <main>
                 <h1>404: PAGE NOT FOUND</h1>
                 <p>Oops, sorry, we can't find this page!</p>
+                <div>
+                    <img src="/images/cute-puppy.jpg" alt= "Floppy Eared Puppy"/>
+                </div>
+                Photo by <a href="https://unsplash.com/@mancity17?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">David Clarke</a> on <a href="https://unsplash.com/images/animals/puppies?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+  
             </main>
         </Def>
     )
